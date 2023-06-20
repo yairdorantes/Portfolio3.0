5201e706194b66dd73aaf4605d4f1a0ee56de377
Yair <yairmasterlol@gmail.com>
2023-06-19 22:57:35 -0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import SliderProjects from "./components/SliderProjects";
+
+function App() {
+  return (
+    <>
+      <SliderProjects></SliderProjects>
+    </>
+  );
+}
+
+export default App;
